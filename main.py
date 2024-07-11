91f9eac39f5a4671dd59b254d92cfa71ccdacd64
Tarique Anwer <itzmetariq@gmail.com>
2024-07-11 18:11:18 +0530

    updated main

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from db.database import engine
+from db.models import Base
+from routers import documents
+import uvicorn
+
+
+# Create the database tables
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(documents.router, prefix="/api", tags=["documents"])
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
