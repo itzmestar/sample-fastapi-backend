c13aa9b7671c3d58f347546785e72840375f5e1f
Tarique Anwer <itzmetariq@gmail.com>
2024-07-11 22:13:35 +0530

    cors disabled

--- main.py
+++ main.py
@@ -3,12 +3,22 @@ from db.database import engine
 from db.models import Base
 from routers import documents
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 
 # Create the database tables
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Allow any origins, methods, and headers for CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow any origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow any method
+    allow_headers=["*"],  # Allow any headers
+)
 
 app.include_router(documents.router, prefix="/api", tags=["documents"])
 
