b91c97b8732d7388c118e5ef6cb866da0680e96e
Tarique Anwer <itzmetariq@gmail.com>
2024-07-11 20:56:15 +0530

    changed ip to 0.0.0.0

--- main.py
+++ main.py
@@ -14,4 +14,4 @@ app.include_router(documents.router, prefix="/api", tags=["documents"])
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
